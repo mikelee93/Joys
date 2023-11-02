3be9a8c25fc5c24aba643e657052a63684649bbf
Mike Lee <114720559+mikelee93@users.noreply.github.com>
2023-11-02 12:30:54 -0400

    Update ConnectionDB.cs

--- Key Return/ConnectionDB.cs
+++ Key Return/ConnectionDB.cs
@@ -8,8 +8,8 @@ namespace Key_Return
 {
     internal class ConnectionDB
     {
-       string con = @"Data Source=192.168.1.21\MDOS;Database=SmartClient_Test;User Id=MSC;Password=j0ysystems!;MultipleActiveResultSets=True;";
-       // string con = @"Data Source=192.168.1.21\MDOS;Database=SmartClient;User Id=MSC;Password=j0ysystems!;MultipleActiveResultSets=True;";
+       string con = @"Data Source=;Database=;User Id=;Password=!;MultipleActiveResultSets=True;";
+       // string con = @"Data Source=;Database=;User Id=;Password=!;MultipleActiveResultSets=True;";
         public string ConnectionString
         {
             get { return con; }
