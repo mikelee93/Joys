d2daa0fee83646607c5cfd5d76b197f955e57ef2
Mike Lee <114720559+mikelee93@users.noreply.github.com>
2023-11-02 12:33:17 -0400

    Update ConnectionDB.cs

--- iQor_Systems/ConnectionDB.cs
+++ iQor_Systems/ConnectionDB.cs
@@ -8,8 +8,8 @@ namespace iQor_Systems
 {
     internal class ConnectionDB
     {
-        string con = @"Data Source=192.168.1.2;Database=testJoy;User Id=sa;Password=lease-return;MultipleActiveResultSets=True;";
-        //string con = @"Data Source=192.168.1.2;Database=JoyData;User Id=sa;Password=lease-return;MultipleActiveResultSets=True;";
+        string con = @"Data Source=;Database=;User Id=;Password=;MultipleActiveResultSets=True;";
+        //string con = @"Data Source=;Database=;User Id=;Password=;MultipleActiveResultSets=True;";
         public string ConnectionString
         {
             get { return con; }
